8d65c8a83e8768b494f1c7d8feedd890b8b7da12
Muhannes <haneda-3@student.ltu.se>
2017-09-05 09:15:22 +0200

    Lookup contact works, with tests

--- golang/kademlia.go
+++ golang/kademlia.go
@@ -1,15 +1,20 @@
 package d7024e
 
-import ("fmt"
-	"encoding/json")
+import (
+	"encoding/json"
+	"fmt"
+)
 
 var Items []string
 
 type Kademlia struct {
+	RT *RoutingTable
+	K  int
 }
 
-func (kademlia *Kademlia) LookupContact(target *Contact) {
-	// TODO
+func (kademlia *Kademlia) LookupContact(target *Contact) []Contact {
+	contacts := kademlia.RT.FindClosestContacts(target.ID, kademlia.K)
+	return contacts
 }
 
 func (kademlia *Kademlia) LookupData(hash string) {
@@ -19,15 +24,15 @@ func (kademlia *Kademlia) LookupData(hash string) {
 func (kademlia *Kademlia) Store(data []byte) {
 	var m Message
 	err := json.Unmarshal(data, &m)
-	if err != nil{
+	if err != nil {
 		fmt.Println("Error when unmarshalling", err)
-	}	
+	}
 	Items = append(Items, string(m.Data))
 	fmt.Println("Store func complete")
 
-	return 
+	return
 }
 
-func (kademlia *Kademlia) GetList() []string{
+func (kademlia *Kademlia) GetList() []string {
 	return Items
 }
--- golang/kademlia_test.go
+++ golang/kademlia_test.go
@@ -1,9 +1,33 @@
 package d7024e
 
 import (
+	"fmt"
 	"testing"
 )
 
 func TestLookupContact(t *testing.T) {
+	_, rt := CreateTestRT()
+
+	contactsCorrect := []Contact{}
+	contactsCorrect = append(contactsCorrect, NewContact(NewKademliaID("FFFFFFFF00000000000000000000000000000000"), "localhost:8001"))
+	contactsCorrect = append(contactsCorrect, NewContact(NewKademliaID("2111111400000000000000000000000000000000"), "localhost:8006"))
+	contactsCorrect = append(contactsCorrect, NewContact(NewKademliaID("1111111400000000000000000000000000000000"), "localhost:8005"))
+	contactsCorrect = append(contactsCorrect, NewContact(NewKademliaID("1111111300000000000000000000000000000000"), "localhost:8004"))
+	contactsCorrect = append(contactsCorrect, NewContact(NewKademliaID("1111111200000000000000000000000000000000"), "localhost:8003"))
+	contactsCorrect = append(contactsCorrect, NewContact(NewKademliaID("1111111100000000000000000000000000000000"), "localhost:8002"))
+
+	kademlia := Kademlia{RT: rt, K: 20}
+	contacts := kademlia.LookupContact(&contactsCorrect[0])
+	fmt.Println(contacts)
+	for i, contact := range contacts {
+		fmt.Println(string(i))
+		if !contact.ID.Equals(contactsCorrect[i].ID) {
+			t.Error("Wrong order in contacts")
+			fmt.Println(contact.ID, contactsCorrect[i].ID)
+		}
+		if i > kademlia.K {
+			t.Error("Too many contacts returned")
+		}
+	}
 
 }
