5b48df61ea077c2669d3e117d3cdd805ed337ee5
Truls Haraldsson <truhar-3@student.ltu.se>
2017-09-05 13:16:28 +0200

    Added test cases for ACKs

--- golang/protocol_test.go
+++ golang/protocol_test.go
@@ -1,42 +1,136 @@
 package d7024e
 
 import (
-	"fmt"
 	"testing"
 )
 
-func TestNewPingMessage(t *testing.T) {
-	var sender = NewKademliaID("FFFFFFFF00000000000000000000000000000000")
+func TestProtocolNewPingMessage(t *testing.T) {
+	var sender = NewKademliaID("ffffffffffffffffffffffffffffffffffffffff")
 	var msg = NewPingMessage(sender)
-	fmt.Println(msg.MsgType)
-	fmt.Println(msg.Sender)
-	fmt.Println(msg.Data)
+	
+	if msg.MsgType != PING {
+		t.Error("Expected message type to be", PING, ", got", msg.MsgType)
+	}
+	if msg.Sender != *sender {
+		t.Error("Expected sender to be", *sender, ", got", msg.Sender)
+	}
 }
 
-func TestNewStoreMessage(t *testing.T) {
-	var sender = NewKademliaID("FFFFFFFF00000000000000000000000000000000")
-	var key = NewKademliaID("AAAAAAAA00000000000000000000000000000000")
+func TestProtocolNewStoreMessage(t *testing.T) {
+	var sender = NewKademliaID("ffffffffffffffffffffffffffffffffffffffff")
+	var key = NewKademliaID("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa")
 	var data = []byte("Data to be stored")
 	var msg = NewStoreMessage(sender, key, &data)
-	fmt.Println(msg.MsgType)
-	fmt.Println(msg.Sender)
-	fmt.Println(msg.Data)
+
+	if msg.MsgType != STORE {
+		t.Error("Expected message type to be", STORE, ", got", msg.MsgType)
+	}
+
+	if msg.Sender != *sender {
+		t.Error("Expected sender to be", *sender, ", got", msg.Sender)
+	}
 }
 
-func TestNewFindNodeMessage(t *testing.T) {
-	var sender = NewKademliaID("FFFFFFFF00000000000000000000000000000000")
-	var nodeID = NewKademliaID("AAAAAAAA00000000000000000000000000000000")
+func TestProtocolNewFindNodeMessage(t *testing.T) {
+	var sender = NewKademliaID("ffffffff00000000000000000000000000000000")
+	var nodeID = NewKademliaID("aaaaaaaa00000000000000000000000000000000")
 	var msg = NewFindNodeMessage(sender, nodeID)
-	fmt.Println(msg.MsgType)
-	fmt.Println(msg.Sender)
-	fmt.Println(msg.Data)
+	
+	if msg.MsgType != FIND_NODE {
+		t.Error("Expected message type to be", FIND_NODE, ", got", msg.MsgType)
+	}
+
+	if msg.Sender != *sender {
+		t.Error("Expected sender to be", *sender, ", got", msg.Sender)
+	}
 }
 
-func TestNewFindValueMessage(t *testing.T) {
-	var sender = NewKademliaID("FFFFFFFF00000000000000000000000000000000")
-	var valueID = NewKademliaID("AAAAAAAA00000000000000000000000000000000")
+func TestProtocolNewFindValueMessage(t *testing.T) {
+	var sender = NewKademliaID("ffffffff00000000000000000000000000000000")
+	var valueID = NewKademliaID("aaaaaaaa00000000000000000000000000000000")
+	
 	var msg = NewFindValueMessage(sender, valueID)
-	fmt.Println(msg.MsgType)
-	fmt.Println(msg.Sender)
-	fmt.Println(msg.Data)
+	if msg.MsgType != FIND_VALUE {
+		t.Error("Expected message type to be", FIND_VALUE, ", got", msg.MsgType)
+	}
+
+	if msg.Sender != *sender {
+		t.Error("Expected sender to be", *sender, ", got", msg.Sender)
+	}
+}
+
+func TestProtocolNewStoreAckMessage(t *testing.T) {
+	var sender = NewKademliaID("FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF")
+	var RPC_ID = NewKademliaID("0000000000000000000000000000000000000000")
+
+	var msg = NewStoreAckMessage(sender, RPC_ID)
+	if msg.MsgType != STORE_ACK {
+		t.Error("Expected message type to be", STORE_ACK, ", got", msg.MsgType)
+	}
+
+	if msg.Sender != *sender {
+		t.Error("Expected sender to be", *sender, ", got", msg.Sender)
+	}
+
+	if msg.RPC_ID != *RPC_ID {
+		t.Error("Expected RPC_ID to be", *RPC_ID, ", got", msg.RPC_ID)
+	}
+}
+
+func TestProtocolNewPingAckMessage(t *testing.T) {
+	var sender = NewKademliaID("FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF")
+	var RPC_ID = NewKademliaID("0000000000000000000000000000000000000000")
+
+	var msg = NewPingAckMessage(sender, RPC_ID)
+	if msg.MsgType != PING_ACK {
+		t.Error("Expected message type to be", PING_ACK, ", got", msg.MsgType)
+	}
+
+	if msg.Sender != *sender {
+		t.Error("Expected sender to be", *sender, ", got", msg.Sender)
+	}
+
+	if msg.RPC_ID != *RPC_ID {
+		t.Error("Expected RPC_ID to be", *RPC_ID, ", got", msg.RPC_ID)
+	}	
+}
+
+
+func TestProtocolNewFindNodeAckMessage(t *testing.T) {
+	var sender = NewKademliaID("FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF")
+	var RPC_ID = NewKademliaID("0000000000000000000000000000000000000000")
+	var nodes = []Contact{}
+
+	var msg = NewFindNodeAckMessage(sender, RPC_ID, &nodes)
+	if msg.MsgType != FIND_NODE_ACK {
+		t.Error("Expected message type to be", FIND_NODE_ACK, ", got", msg.MsgType)
+	}
+
+	if msg.Sender != *sender {
+		t.Error("Expected sender to be", *sender, ", got", msg.Sender)
+	}
+
+	if msg.RPC_ID != *RPC_ID {
+		t.Error("Expected RPC_ID to be", *RPC_ID, ", got", msg.RPC_ID)
+	}	
+}
+
+
+func TestProtocolNewFindValueAckMessage(t *testing.T) {
+	var sender = NewKademliaID("FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF")
+	var RPC_ID = NewKademliaID("0000000000000000000000000000000000000000")
+	var value = []byte("This is data")
+
+	var msg = NewFindValueAckMessage(sender, RPC_ID, &value)
+	if msg.MsgType != FIND_VALUE_ACK {
+		t.Error("Expected message type to be", FIND_VALUE_ACK, ", got", msg.MsgType)
+	}
+
+	if msg.Sender != *sender {
+		t.Error("Expected sender to be", *sender, ", got", msg.Sender)
+	}
+
+	if msg.RPC_ID != *RPC_ID {
+		t.Error("Expected RPC_ID to be", *RPC_ID, ", got", msg.RPC_ID)
+	}	
 }
