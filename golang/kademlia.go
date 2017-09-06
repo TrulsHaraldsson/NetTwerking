7cb324c9ca850cdb088d47f7323ee12a9e4dd5a2
tedjoh-2 <tedjoh-2@student.ltu.se>
2017-09-06 10:14:43 +0200

    Fixed Kademlia/test.

--- golang/kademlia.go
+++ golang/kademlia.go
@@ -3,10 +3,10 @@ package d7024e
 import (
 	"encoding/json"
 	"fmt"
+	"reflect"
 )
 
-var Items []string
-//var Items []Item
+var Information []Item
 
 type Item struct{
 	Value string
@@ -23,22 +23,24 @@ func (kademlia *Kademlia) LookupContact(target *Contact) []Contact {
 	return contacts
 }
 
-func (kademlia *Kademlia) LookupData(hash string) {
+func (kademlia *Kademlia) LookupData(hash *KademliaID){
 	// TODO
-	var foundData string
+	fmt.Println("Hash search for : ", hash)
+	fmt.Println("Hash search for *: ", *hash)
+	fmt.Println("Hash type : ", reflect.TypeOf(hash))
 	found := false
 	
-	for _, v := range Items{
-		if v == hash {
-			foundData = v
+	for _, v := range Information{
+		if v.Key == *hash{
+			fmt.Println("Found item", v.Key)
 			found = true
 		}
 	}
-	
-	if found == true {
-		fmt.Println("Found value : ", foundData)
+	if found == true{
+		fmt.Println("Successful search for item, returning.\n")
+		return
 	}else{
-		// CHECK OUT NODES 
+		fmt.Println("Failed search for item, keep searching.\n")
 		/*
 		ch := make(chan []byte, 3)
 		contacts := kademlia.RT.FindClosestContacts(target.ID, kademlia.K)
@@ -47,36 +49,38 @@ func (kademlia *Kademlia) LookupData(hash string) {
 			go func(){
 				
 			}()
-		}
-		*/
-		fmt.Println("No value found")
+		}*/
+		return
 	}
-}
+}	
 
 func (kademlia *Kademlia) Store(data []byte) {
-	
-	fmt.Println("Entire data : ", string(data))
-	
-	var m Message
+	var m StoreMessage
 	err := json.Unmarshal(data, &m)
 	if err != nil {
 		fmt.Println("Error when unmarshalling", err)
 	}
-	fmt.Println("Type : ", m.MsgType)
-	fmt.Println("Sender : ")
-	fmt.Println("Data : ")
-/*		MsgType string
-	Sender  KademliaID
-	RPC_ID KademliaID
-	Data    []byte
-*/
-	//item := Item{}
-	//newItem := item(m.Key
-	Items = append(Items, string(m.Data))
-	fmt.Println("Store func complete")
+	item := Item{string(m.Data), m.Key}
+	Information = append(Information, item)	
+	fmt.Println("Store func complete.\n")
 	return
 }
 
-func (kademlia *Kademlia) GetList() []string {
-	return Items
+func (kademlia *Kademlia) getInformation() []Item {
+	return Information
 }
+/*
+func (kademlia *Kademlia) removeInformation(hash KademliaID){
+	found := false	
+	for i, v := range Information{
+		if v.Key == hash {
+			Information = append(Information[:i],Information[i + 1:]...)
+			found = true
+		}
+	}
+	if found == true {
+		fmt.Println("Item Deleted!\n", Information)
+	}else{
+		fmt.Println("No value found")
+	}
+}*/
