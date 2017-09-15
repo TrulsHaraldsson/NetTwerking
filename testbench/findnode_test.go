6e9b4a534c32c9028839718ba2002dc876ce973d
tedjoh-2 <tedjoh-2@student.ltu.se>
2017-09-15 17:25:30 +0200

    Fix

--- testbench/findnode_test.go
+++ testbench/findnode_test.go
@@ -5,13 +5,15 @@ import (
 	"testing"
 
 	"../golang"
+	"time"
 )
 
 func TestFindNode(t *testing.T) {
-	d7024e.StartNode(8100, "none", "none")
-	n1 := d7024e.StartNode(8101, "localhost:8100", "none")
-	d7024e.StartNode(8102, "localhost:8100", "none")
-	d7024e.StartNode(8103, "localhost:8102", "2111111400000000000000000000000000000000")
+	d7024e.StartNode(8100, "none", "none") //W
+	time.Sleep(50 * time.Millisecond)
+	n1 := d7024e.StartNode(8101, "localhost:8100", "none") // Q -> W
+	d7024e.StartNode(8102, "localhost:8100", "none") // S -> W
+	d7024e.StartNode(8103, "localhost:8102", "2111111400000000000000000000000000000000") // A -> S
 	fmt.Println("All nodes connected", n1)
 	contact := n1.SendFindContactMessage(d7024e.NewKademliaID("2111111400000000000000000000000000000000"))
 
