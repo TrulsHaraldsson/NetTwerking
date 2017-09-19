2f266524881d03f45d4d820ad464c092c11450f9
Truls Haraldsson <truhar-3@student.ltu.se>
2017-09-19 13:19:37 +0200

    Code cleaning

--- golang/routingtable_test.go
+++ golang/routingtable_test.go
@@ -15,6 +15,29 @@ func TestRoutingTable(t *testing.T) {
 	}
 }
 
+
+func TestRoutingTableAddContact(t *testing.T) {
+	rt := NewRoutingTable(NewContact(
+		NewKademliaID("FFFFFFFF00000000000000000000000000000000"), "localhost:8000"))
+	contacts := []Contact{}
+	contacts = append(contacts, NewContact(
+		NewKademliaID("FFFFFFFF00000000000000000000000000000000"), "localhost:8000"))
+	contacts = append(contacts, NewContact(
+		NewKademliaID("1111111100000000000000000000000000000000"), "localhost:8002"))
+	contacts = append(contacts, NewContact(
+		NewKademliaID("1111111200000000000000000000000000000000"), "localhost:8003"))
+	contacts = append(contacts, NewContact(
+		NewKademliaID("1111111300000000000000000000000000000000"), "localhost:8004"))
+	contacts = append(contacts, NewContact(
+		NewKademliaID("1111111400000000000000000000000000000000"), "localhost:8005"))
+	contacts = append(contacts, NewContact(
+		NewKademliaID("2111111400000000000000000000000000000000"), "localhost:8006"))
+
+	for _, contact := range contacts {
+		go rt.AddContact(contact)
+	}
+}
+
 func CreateTestRT() ([]Contact, *RoutingTable) {
 	rt := NewRoutingTable(NewContact(
 		NewKademliaID("FFFFFFFF00000000000000000000000000000000"), "localhost:8000"))
