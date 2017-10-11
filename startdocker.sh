2bbc851dbbd69c6a18e68e777bcca2b435cdeaaa
Muhannes <haneda-3@student.ltu.se>
2017-10-11 12:37:10 +0200

    changed in startdocker.sh

--- startdocker.sh
+++ startdocker.sh
@@ -4,7 +4,6 @@ docker build -t follower -f Dockerfile-follower .
 screen -d -m bash -c "docker run -it interactive"
 sleep 2
 for (( i = 0; i < 10; i++ )); do
-  echo "Starting one follower"
   docker run -d follower
 done
 screen -r
